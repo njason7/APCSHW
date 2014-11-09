f50021cce91dc206732c321028fbe6440b5cf0bb
Jason Ng <n.jason7@gmail.com>
2014-11-08 20:25:12 -0500

    Made set and remove.

--- SuperArray.java
+++ SuperArray.java
@@ -37,46 +37,44 @@ public class SuperArray{
     }
     
     public void add(int index, Object o){
-	if (!OutOfRange(index)){
-	    if (data[index] == null){
-		data[index] = o;
-		stored++;
-	    }else if (data[data.length-1] != null){
-		Object[] result = new Object[data.length + 1];
-		int i = 0;
-		while (i<index){
-		    result[i] = data[i];
-		    i++;
-		}
-		result[i] = o;
-		while (i<data.length){
-		    result[i+1] = data[i];
-		    i++;
-		}
-		data = result;
-		stored++;
-	    }else{
-		Object[] result = new Object[data.length];
-		int i = 0;
-		while (i<index){
-		    result[i] = data[i];
-		    i++;
-		}
-		result[i] = o;
-		while (i<data.length-1){
-		    result[i+1] = data[i];
-		    i++;
-		}
-		data = result;
-		stored++;
+	if (data[index] == null){
+	    data[index] = o;
+	    stored++;
+	}else if (data[data.length-1] != null){
+	    Object[] result = new Object[data.length + 1];
+	    int i = 0;
+	    while (i<index){
+		result[i] = data[i];
+		i++;
 	    }
+	    result[i] = o;
+	    while (i<data.length){
+		result[i+1] = data[i];
+		i++;
+	    }
+	    data = result;
+	    stored++;
+	}else{
+	    Object[] result = new Object[data.length];
+	    int i = 0;
+	    while (i<index){
+		result[i] = data[i];
+		i++;
+	    }
+	    result[i] = o;
+	    while (i<data.length-1){
+		result[i+1] = data[i];
+		i++;
+	    }
+	    data = result;
+	    stored++;
 	}
     }
-	
+    
     public boolean OutOfRange(int ind){
 	return (ind <  0 || ind >= size());
     }
-
+    
     public Object get(int index){
 	if (!OutOfRange(index)){
 	    return data[index];
@@ -85,12 +83,12 @@ public class SuperArray{
 	    return null;
 	}
     }
-	
-
+    
+    
     public int size(){
 	return data.length;
     }
-
+    
     public void resize(int newCapacity){
 	if (newCapacity > data.length){
 	Object[]result = new Object[newCapacity];
@@ -98,6 +96,39 @@ public class SuperArray{
 	    result[i] = data[i];
 	}
 	data = result;
+	}
+    }
+
+    public Object set(int index, Object o){
+	if (!OutOfRange(index)){
+	    Object save = data[index];
+	    data[index] = o;
+	    return save;
+	}else{
+	    System.out.println("ERROR. INDEX OUT OF RANGE.");
+	    return null;
+	}
+    }
+
+    public Object remove(int index){
+	if(!OutOfRange(index)){
+	    Object save = data[index];
+	    Object[] result = new Object[data.length];
+	    int i = 0;
+	    while (i<index){
+		result[i] = data[i];
+		i++;
+	    }
+	    while (i+1<data.length){
+		result[i] = data[i+1];
+		i++;
+	    }
+	    data = result;
+	    stored--;
+	    return save;
+	}else{
+	    System.out.println("ERROR. INDEX OUT OF RANGE.");
+	    return null;
 	}
     }
 
@@ -117,5 +148,11 @@ public class SuperArray{
 	System.out.println(test);
 	System.out.println(test.get(0));
 	System.out.println(test.get(11));
+	System.out.println(test.set(0,"u"));
+	System.out.println(test);
+	System.out.println(test.set(11,"u"));	
+	System.out.println(test.remove(0));
+	System.out.println(test);	
+	System.out.println(test.remove(11));
     }
 }
