7812d443c7ffbcbba256b677140cf05bb1a32e89
Jason Ng <n.jason7@gmail.com>
2014-12-02 16:49:56 -0500

    Forgot to commit yesterday.

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -10,7 +10,7 @@ public class OrderedSuperArray extends SuperArray{
 
     public int getProper(String e){
 	for (int i = 0;i<data.length;i++){
-	    if (e.compareTo(data[i]) > 0){
+	    if (data[i] == null || e.compareTo(data[i]) < 0){
 		return i;
 	    }
 	}
@@ -18,11 +18,28 @@ public class OrderedSuperArray extends SuperArray{
     }
     
     public void add (String e){
-	
-    }	
+	int index = getProper(e);
+	super.add(index,e);
+    }
+
+    public void add(int index,String e){
+	add(e);
+    }
+    
+    public String set(int index,String o){
+	String save = super.remove(index);
+	super.add(o);
+	return save;
+    }
 
     public static void main(String[]args){
 	OrderedSuperArray test = new OrderedSuperArray();
+	test.add("jkl");
+	test.add("abc");
+	test.add("def");
+	test.add("ghi");
+	System.out.println(test);
+	System.out.println(test.set(0,"mno"));
 	System.out.println(test);
     }
 }
